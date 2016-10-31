2cb814e373e3c347143fbeee5a3700e7a12080b6
LeopoldArkham <leopold.arkham@gmail.com>
2016-10-31 18:08:02 +0100

    Added normalization + tests

--- src/bool.rs
+++ src/bool.rs
@@ -3,15 +3,20 @@ static TO_TRUE: [&'static str; 9] = ["1", "yes", "y", "ok", "okay", "continue", 
 
 static TO_FALSE: [&'static str; 7] = ["0", "no", "n", "stop", "end", "off", "false"];
 
+fn normalize(source: &str) -> String {
+    source.trim().to_lowercase()
+}
+
 pub trait ToBool {
     fn to_bool(&self) -> Option<bool>;
 }
 
 impl ToBool for &'static str {
     fn to_bool(&self) -> Option<bool> {
-        if TO_TRUE.iter().any(|x| x == self) {
+        let s = &normalize(self);
+        if TO_TRUE.iter().any(|x| x == s) {
             Some(true)
-        } else if TO_FALSE.iter().any(|x| x == self) {
+        } else if TO_FALSE.iter().any(|x| x == s) {
             Some(false)
         } else {
             None
@@ -21,9 +26,10 @@ impl ToBool for &'static str {
 
 impl ToBool for String {
     fn to_bool(&self) -> Option<bool> {
-        if TO_TRUE.iter().any(|x| x == self) {
+        let s = &normalize(self);
+        if TO_TRUE.iter().any(|x| x == s) {
             Some(true)
-        } else if TO_FALSE.iter().any(|x| x == self) {
+        } else if TO_FALSE.iter().any(|x| x == s) {
             Some(false)
         } else {
             None
@@ -39,7 +45,7 @@ impl Humanize for bool {
     fn humanize(&self) -> &str {
         match *self {
             true => "yes",
-            false => "false",
+            false => "no",
         }
     }
 }
@@ -49,6 +55,11 @@ fn str_to_bool() {
     assert_eq!("yes".to_bool(), Some(true));
     assert_eq!("stop".to_bool(), Some(false));
 }
+#[test]
+fn str_to_bool_needs_normalizing() {
+    assert_eq!("\tYES".to_bool(), Some(true));
+    assert_eq!("Stop  ".to_bool(), Some(false));
+}
 
 #[test]
 fn string_to_bool() {
@@ -56,6 +67,15 @@ fn string_to_bool() {
     assert_eq!(s.to_bool(), Some(true));
 
     let s2 = String::from("stop");
+    assert_eq!(s2.to_bool(), Some(false));
+}
+
+#[test]
+fn string_to_bool_needs_normalizing() {
+    let s = String::from("\nOK");
+    assert_eq!(s.to_bool(), Some(true));
+
+    let s2 = String::from(" Off");
     assert_eq!(s2.to_bool(), Some(false));
 }
 

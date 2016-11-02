2aaa66d49af4f3466c4f6011c62ccb6237bf6eae
LeopoldArkham <leopold.arkham@gmail.com>
2016-11-02 19:18:43 +0100

    Finished ordinals

--- src/numbers.rs
+++ src/numbers.rs
@@ -1,82 +1,101 @@
 #[allow(unused_imports)]
 use super::humanize::Humanize;
+use std::fmt::Write;
 
 
 trait HumanizeNumbers {
-    fn ord(&self) -> &str;
+    fn ord(&self) -> String;
 }
 
 impl HumanizeNumbers for u8 {
-    fn ord(&self) -> &str {
+    fn ord(&self) -> String {
         let suffix = match self % 10 {
         	1 => "st",
         	2 => "nd",
         	3 => "rd",
         	_ => "th"
         };
+        let mut res = String::new();
+        let _ = write!(res, "{}{}", self, suffix);
+        res
     }
 }
 
 impl HumanizeNumbers for i8 {
-    fn ord(&self) -> &str {
+    fn ord(&self) -> String {
         let suffix = match self % 10 {
         	1 => "st",
         	2 => "nd",
         	3 => "rd",
         	_ => "th"
         };
+        let mut res = String::new();
+        let _ = write!(res, "{}{}", self, suffix);
+        res
     }
 }
 
 impl HumanizeNumbers for u16 {
-    fn ord(&self) -> &str {
+    fn ord(&self) -> String {
         let suffix = match self % 10 {
         	1 => "st",
         	2 => "nd",
         	3 => "rd",
         	_ => "th"
         };
+        let mut res = String::new();
+        let _ = write!(res, "{}{}", self, suffix);
+        res
     }
 }
 
 impl HumanizeNumbers for i16 {
-    fn ord(&self) -> &str {
+    fn ord(&self) -> String {
         let suffix = match self % 10 {
         	1 => "st",
         	2 => "nd",
         	3 => "rd",
         	_ => "th"
         };
+        let mut res = String::new();
+        let _ = write!(res, "{}{}", self, suffix);
+        res
     }
 }
 
 impl HumanizeNumbers for u32 {
-    fn ord(&self) -> &str {
+    fn ord(&self) -> String {
         let suffix = match self % 10 {
         	1 => "st",
         	2 => "nd",
         	3 => "rd",
         	_ => "th"
         };
+        let mut res = String::new();
+        let _ = write!(res, "{}{}", self, suffix);
+        res
     }
 }
 
 impl HumanizeNumbers for i32 {
-    fn ord(&self) -> &str {
+    fn ord(&self) -> String {
         let suffix = match self % 10 {
         	1 => "st",
         	2 => "nd",
         	3 => "rd",
         	_ => "th"
         };
+        let mut res = String::new();
+        let _ = write!(res, "{}{}", self, suffix);
+        res
     }
 }
 
 #[test]
 fn test_ordinals() {
-	assert_eq!(101.ord(), "st");
-	assert_eq!(2.ord(), "nd");
-	assert_eq!(10093.ord(), "rd");
-	assert_eq!((-159652).ord(), "th");
-	assert_eq!(0.ord(), "th");
+	assert_eq!(101.ord(), "101st");
+	assert_eq!(2.ord(), "2nd");
+	assert_eq!(10093.ord(), "10093rd");
+	assert_eq!((-159652).ord(), "-159652th");
+	assert_eq!(0.ord(), "0th");
 }

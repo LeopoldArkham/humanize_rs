34186b90c987bc32778a15bcfb6421c9f9ede972
LeopoldArkham <leopold.arkham@gmail.com>
2016-11-04 23:40:31 +0100

    Revert here when everything dies

--- examples/numbers.rs
+++ examples/numbers.rs
@@ -1,12 +1,17 @@
 extern crate humanize;
+#[allow(unused_imports)]
+use humanize::numbers::{HumanizeNumbers, to_text};
 
-use humanize::numbers::HumanizeNumbers;
+fn test_to_string(num: u64) {
+}
 
 fn main() {
-	let scoreboard = ["Bob", "Victor", "Richard", "John",
-	"Lisa"];
+	// let scoreboard = ["Bob", "Victor", "Richard", "John",
+	// "Lisa"];
 
-	for (i, name) in scoreboard.iter().enumerate() {
-		println!("{}: {}", (i+1).ord(), name);
-	}
+	// for (i, name) in scoreboard.iter().enumerate() {
+	// 	println!("{}: {}", (i+1).ord(), name);
+	// }
+	test_to_string(num);
+
 }

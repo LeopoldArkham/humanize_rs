0d142182ef46580f0b8c82ab842028192b622134
LeopoldArkham <leopold.arkham@gmail.com>
2016-11-14 17:13:10 +0100

    Better example

--- examples/numbers.rs
+++ examples/numbers.rs
@@ -2,12 +2,13 @@ extern crate humanize;
 use humanize::numbers::{HumanizeNumbers};
 
 fn main() {
-	// let scoreboard = ["Bob", "Victor", "Richard", "John",
-	// "Lisa"];
+	let scoreboard = ["Bob", "Victor", "Richard", "John",
+	"Lisa"];
 
-	// for (i, name) in scoreboard.iter().enumerate() {
-	// 	println!("{}: {}", (i+1).ord(), name);
-	// }
+	for (i, name) in scoreboard.iter().enumerate() {
+		println!("{}: {}", (i+1).ord(), name);
+	}
+	
 	let num: u64 = 666666666;
 	println!("{}: {}", num, num.to_text());
 }
--- src/numbers.rs
+++ src/numbers.rs
@@ -34,52 +34,50 @@ pub trait HumanizeNumbers {
 
 fn stringify(res: &mut String, chunk: Vec<usize>) {
     match chunk.len() {
-    	3 => {
-    		match chunk[1] {
-    			1 => {
-    				*res += DIGITS[chunk[0] as usize];
-            		*res += " hundred and ";
-            		*res += TO_20[chunk[2] as usize];
-    			}
-    			0 => {
-    				*res += DIGITS[chunk[0] as usize];
-            		*res += " hundred";
-            		if chunk[2] != 0 {
-                		*res += " and ";
-                		*res += DIGITS[chunk[2] as usize]
-            		}
-    			}
-    			_ => {
-    				if chunk[0] != 0 {
-	    				*res += DIGITS[chunk[0] as usize];
-	            		*res += " hundred and ";
-	            	}
-	            	*res += TENS[chunk[1] as usize];
-            		if chunk[2] != 0 {
-                		*res += "-";
-                		*res += DIGITS[chunk[2] as usize]
-            		}
-    			}
-    		}
-    	}
-    	2 => {
-    		match chunk[0] {
-    			1 => {
-    				*res += TO_20[chunk[1] as usize]
-    			}
-    			_ => {
-    				*res += TENS[chunk[0] as usize];
-            		if chunk[1] != 0 {
-                		*res += "-";
-                		*res += DIGITS[chunk[1] as usize]
-            		}
-        		}
-    		}
-    	}
-    	1 => {
-    		*res += DIGITS[chunk[0] as usize];
-    	}
-    	_ => unreachable!{}
+        3 => {
+            match chunk[1] {
+                1 => {
+                    *res += DIGITS[chunk[0] as usize];
+                    *res += " hundred and ";
+                    *res += TO_20[chunk[2] as usize];
+                }
+                0 => {
+                    *res += DIGITS[chunk[0] as usize];
+                    *res += " hundred";
+                    if chunk[2] != 0 {
+                        *res += " and ";
+                        *res += DIGITS[chunk[2] as usize]
+                    }
+                }
+                _ => {
+                    if chunk[0] != 0 {
+                        *res += DIGITS[chunk[0] as usize];
+                        *res += " hundred and ";
+                    }
+                    *res += TENS[chunk[1] as usize];
+                    if chunk[2] != 0 {
+                        *res += "-";
+                        *res += DIGITS[chunk[2] as usize]
+                    }
+                }
+            }
+        }
+        2 => {
+            match chunk[0] {
+                1 => *res += TO_20[chunk[1] as usize],
+                _ => {
+                    *res += TENS[chunk[0] as usize];
+                    if chunk[1] != 0 {
+                        *res += "-";
+                        *res += DIGITS[chunk[1] as usize]
+                    }
+                }
+            }
+        }
+        1 => {
+            *res += DIGITS[chunk[0] as usize];
+        }
+        _ => unreachable!{},
     }
 }
 
@@ -87,47 +85,43 @@ macro_rules! impl_humanize_numbers_u {
     ($name:ident for $($t:ty)*) => ($(
         impl $name for $t {
             fn ord(&self) -> String {
-        		let suffix = match self % 10 {
-            		1 => "st",
-            		2 => "nd",
-            		3 => "rd",
-            		_ => "th",
-        		};
-        		let mut res = String::new();
-        		let _ = write!(res, "{}{}", self, suffix);
-        		res
-    		}
+                let suffix = match self % 10 {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th",
+                };
+                let mut res = String::new();
+                let _ = write!(res, "{}{}", self, suffix);
+                res
+            }
 
-    		fn to_text(&self) -> String {
-    			if *self == 0 {
-    			    return "zero".to_string();
-    			}
-    			let mut num = *self;
-    			
-    			let mut split_digits = Vec::new();
-    			// let mut divider: u64 = 10;
-			
-    			while num > 0 {
-    			    // let cur = num % 10;
-    			    split_digits.insert(0, (num % 10) as usize);
-    			    num /= 10
-    			    // divider *= 10;
-    			}
-    				
-				let (first, remainder) = split_digits.split_at(split_digits.len() % 3);
-				let chunks = first.chunks(3).chain(remainder.chunks(3)).map(|x| x.to_vec()).collect::<Vec<_>>();
-			
-				let mut res = String::new();
-				let mut scale_idx = chunks.len();
-			
-				for c in chunks {
-					stringify(&mut res, c);
-					scale_idx -= 1;
-					res += SCALE[scale_idx];
-				}
-			
-				res
-}
+            fn to_text(&self) -> String {
+                if *self == 0 {
+                    return "zero".to_string();
+                }
+                let mut num = *self;
+                let mut split_digits = Vec::new();
+            
+                while num > 0 {
+                    split_digits.insert(0, (num % 10) as usize);
+                    num /= 10
+                }
+                    
+                let (first, remainder) = split_digits.split_at(split_digits.len() % 3);
+                let chunks = first.chunks(3).chain(remainder.chunks(3)).map(|x| x.to_vec()).collect::<Vec<_>>();
+            
+                let mut res = String::new();
+                let mut scale_idx = chunks.len();
+            
+                for c in chunks {
+                    stringify(&mut res, c);
+                    scale_idx -= 1;
+                    res += SCALE[scale_idx];
+                }
+            
+                res
+            }
         }
     )*)
 }
@@ -136,16 +130,16 @@ macro_rules! impl_humanize_numbers_i {
     ($name:ident for $($t:ty)*) => ($(
         impl $name for $t {
             fn ord(&self) -> String {
-            	if *self < 0 {
-        			"-".to_string() + &(self.abs() as u64).ord()
-        		} else {
-        			(*self as u64).ord()
-        		}
-    		}
+                if *self < 0 {
+                    "-".to_string() + &(self.abs() as u64).ord()
+                } else {
+                    (*self as u64).ord()
+                }
+            }
 
-    		fn to_text(&self) -> String {
-    			(self.abs() as u64).to_text()
-    		}
+            fn to_text(&self) -> String {
+                (self.abs() as u64).to_text()
+            }
 
         }
     )*)

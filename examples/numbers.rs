dcbcab0e018d14de954660a2f656e0e1fe3294ef
LeopoldArkham <leopold.arkham@gmail.com>
2016-12-09 04:11:45 +0100

    Publicly export HN trait

--- examples/numbers.rs
+++ examples/numbers.rs
@@ -1,14 +1,15 @@
 extern crate humanize;
-use humanize::numbers::{HumanizeNumbers};
+use humanize::HumanizeNumbers;
 
 fn main() {
-	let scoreboard = ["Bob", "Victor", "Richard", "John",
-	"Lisa"];
+    let scoreboard = ["Bob", "Victor", "Richard", "John", "Lisa"];
 
-	for (i, name) in scoreboard.iter().enumerate() {
-		println!("{}: {}", (i+1).ord(), name);
-	}
-	
-	let num: i64 = -6;
-	println!("{}: {}", num, num.to_text());
+    for (i, name) in scoreboard.iter().enumerate() {
+        println!("{}: {}", (i + 1).ord(), name);
+    }
+
+    let num: i64 = -6;
+    println!("{}: {}", num, num.to_text());
+
+    println!("{}", 0.intcomma());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod bool;
 pub mod numbers;
 pub mod humanize;
+
+pub use numbers::HumanizeNumbers;
